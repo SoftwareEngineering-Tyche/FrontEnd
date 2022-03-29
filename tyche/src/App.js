b0eae1947775e2be37ff0826766355549613fad8
Ali Fata <a.fata@pinket.com>
2022-03-29 13:27:28 +0430

    footer design completed

--- tyche/src/App.js
+++ tyche/src/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 import './App.css';
 import HomePage from './pages/home-page';
 import LoginPage from './pages/login-page';
 import Header from './components/header';
 import Footer from './components/footer';
 
+const cacheRtl = createCache({ key: "muirtl", stylisPlugins: [prefixer, rtlPlugin] });
+
 function App() {
+
+  React.useLayoutEffect(() => {
+    document.body.setAttribute("dir", "rtl");
+  }, []);
+  const theme = createTheme({
+    direction: 'rtl', 
+    padding:'0px', 
+    typography: {
+      "fontFamily": `"Yekan", "Arial", sans-serif`,
+    }
+  });
+
   return (
-    <div className="App">
-      <Header/>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/login' element={<LoginPage/>} />
-        </Routes>
-      </BrowserRouter>
-      <Footer/>
-    </div>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <div className="App" dir="rtl">
+          <Header/>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<HomePage/>} />
+              <Route path='/login' element={<LoginPage/>} />
+            </Routes>
+          </BrowserRouter>
+          <Footer/>
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
--- tyche/src/components/footer.jsx
+++ tyche/src/components/footer.jsx
@@ -1,34 +1,146 @@
-import React, { useRef } from "react";
+import React, { memo, useState } from "react";
 import "../assets/styles/footer.scss";
 import Button from '@mui/material/Button';
 import walletIcon from '../assets/images/wallet.svg';
 import collectionsIcon from '../assets/images/collection.svg';
 import nftIcon from '../assets/images/nft.svg';
 import saleIcon from '../assets/images/sale.svg';
+import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import imageSample from "../assets/images/image.png";
+import { Divider } from "@mui/material";
 
-function Footer () {
-    
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+function Footer() {
+
+    const [email, setEmail] = useState(undefined);
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [pressSubmit, setPressSubmit] = useState(false);
+
+    const emailRegex = RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+
+    const checkEmail = (email) => {
+        const res = emailRegex.test(email)
+        setIsEmailValid(res);
+    }
+
+    const getOptions1 = () => {
+        return (<>
+            <div className="option">
+                <div className="icon-container"><img src={walletIcon} className="icon" /></div>
+                <span>دسترسی مستقیم به کیف پول</span>
+            </div>
+            <div className="option">
+                <div className="icon-container"><img src={collectionsIcon} className="icon" /></div>
+                <span>ساخت مجموعه‌های متنوع</span>
+            </div>
+        </>);
+    }
+    const getOptions2 = () => {
+        return (<>
+            <div className="option">
+                <div className="icon-container"><img src={nftIcon} className="icon" /></div>
+                <span>اضاقه کردن آثار دلخواه</span>
+            </div>
+            <div className="option">
+                <div className="icon-container"><img src={saleIcon} className="icon" /></div>
+                <span>لیست کردن آثار برای فروش</span>
+            </div>
+        </>);
+    }
+    const getCustomerServices = () => {
+        return (
+            <div className="services-container">
+                <div>خدمات مشتریان</div>
+                <div className="services">
+                    <span className="service">پاسخ به پرسش‌های متداول</span>
+                    <span className="service">حریم خصوصی</span>
+                    <span className="service">شرایط استفاده</span>
+                    <span className="service">گزارش باگ</span>
+                </div>
+            </div>
+        );
+    }
+    const getMyAccountServices = () => {
+        return (
+            <div className="services-container">
+                <div>حساب کاربری من</div>
+                <div className="services">
+                    <span className="service">پروفایل</span>
+                    <span className="service">علاقه‌مندی‌‌ها</span>
+                    <span className="service">کیف پول</span>
+                    <span className="service">دارایی‌ها</span>
+                </div>
+            </div>
+        );
+    }
     return (
         <div className="footer">
             <div className="options">
-                <div className="option">
-                    <img src={walletIcon} className="icon"/>
-                    <span>کیف پولت را داشته باش</span>
+                {window.innerWidth <= 768 ? <>
+                    <div>{getOptions1()}</div>
+                    <div>{getOptions2()}</div>
+                </>
+                : <> {getOptions1()} {getOptions2()} </>}
+            </div>
+            <Divider/>
+            <div className="actions-container">
+                <div className="tyche-info">
+                    <div className="title">
+                        <img src={imageSample} width={36} height={36} />
+                        <span className="title-txt">تایکی</span>
+                    </div>
+                    <div className="sub-title">
+                        اولین و بزرگ‌ترین پلتفرم ایرانی در حوزه توکن‌های غیر قابل تعویض (NFT) برای جمع‌آوری، خرید، فروش و جستجو در اقلام دیجیتال منحصر به فرد
+                    </div>
                 </div>
-                <div className="option">
-                    <img src={collectionsIcon} className="icon"/>
-                    <span>کیف پولت را داشته باش</span>
+                {window.innerWidth <= 768 ? <><div style={{display:'flex'}}>{getMyAccountServices()} {getCustomerServices()}</div></>
+                : <> {getMyAccountServices()} {getCustomerServices()} </>}
+                <div>
+                    <div className="user-email">
+                        <div>از آخرین اخبار تایکی مطلع شوید</div>
+                        <div className="email-form">
+                            <input
+                                type="text"
+                                placeholder="ایمیل خود را وارد کنید"
+                                value={email}
+                                onChange={event => { setEmail(event.target.value); checkEmail(event.target.value); }}
+                                className="email-input"
+                            />
+                            <Button classes={{ root: 'btn', disabled: 'disabled-btn' }} disabled={!email} onClick={() => setPressSubmit(true)}>
+                                ثبت
+                            </Button>
+                            {isEmailValid && <Snackbar open={pressSubmit} autoHideDuration={3000} onClose={() => setPressSubmit(false)}>
+                                <Alert onClose={() => setPressSubmit(false)} severity="success" sx={{ width: '100%' }}>
+                                    ایمیل شما با موفقیت ثبت شد!
+                                </Alert>
+                            </Snackbar>}
+                            {!isEmailValid && <Snackbar open={pressSubmit} autoHideDuration={3000} onClose={() => setPressSubmit(false)}>
+                                <Alert onClose={() => setPressSubmit(false)} severity="error" sx={{ width: '100%' }}>
+                                    آدرس ایمیل وارد شده نامعتبر است!
+                                </Alert>
+                            </Snackbar>}
+                        </div>
+                    </div>
+                    <div className="contact">
+                        ارتباط با ما
+                        <div className="contact-info">
+                            <EmailRoundedIcon classes={{ root: "email-icon" }} />
+                            <a href="mailto:contact.tyche.team@gmail.com" className="email-address">
+                                <span>contact.tyche.team@gmail.com</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div className="option">
-                    <img src={nftIcon} className="icon"/>
-                    <span>کیف پولت را داشته باش</span>
-                </div>
-                <div className="option">
-                    <img src={saleIcon} className="icon"/>
-                    <span>کیف پولت را داشته باش</span>
-                </div>
+            </div>
+            <div className="copy-right">
+                ۱۴۰۰-۱۴۰۱ کپی‌رایت © تمامی حقوق محفوظ است
             </div>
         </div>
     );
 }
-export default Footer;
+export default memo(Footer);
