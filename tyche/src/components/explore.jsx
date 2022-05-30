9eb3a74b0eec5a21afda0f29d0a8b58b6b97cae2
zahra1378 <zahra.m346@gmail.com>
2022-05-31 03:49:29 +0430

    filter and sort explore

--- tyche/src/components/explore.jsx
+++ tyche/src/components/explore.jsx
@@ -3,27 +3,100 @@ import { Link } from 'react-router-dom';
 import { Grid, Tab, Tabs, Container, Card } from '@mui/material';
 import { hostUrl } from '../host-url';
 import { callAPI } from "../components/api-call";
+import { margin, textAlign } from '@mui/system';
+import { Input, Button, TextField } from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const Explore = () => {
 
     const [tab, setTab] = useState('all');
+    const [min, setMin] = useState(null);
+    const [max, setMax] = useState(null);
     const [Resault, setResault] = useState([]);
+    const [Sortcollection, setSortcollection] = useState([]);
+    const [Sortnftprice, setSortnftprice] = useState([]);
+    const [Filterresualt, setFilterresualt] = useState([]);
 
     useEffect(() => {
         const data = new FormData();
         data.append("hotest", "true");
         data.append("favorites", "true");
         data.append("latest", "true");
-        callAPI({ method: 'POST', url: `${hostUrl}/explore`, data: data }).then(response => {
+        callAPI({ method: 'POST', url: `hostUrl/explore`, data: data }).then(response => {
             setResault(response.payload);
         });
     }, []);
+   const useEffectSortCollection=() => {
+        const data = new FormData();
+        data.append("params", "liked");
+        data.append("kind", "ascending");
+        callAPI({ method: 'POST', url: `hostUrl/sortCollection`, data: data }).then(response => {
+            setSortcollection(response.payload);
+        });
+    };
+   const useEffectSortNFT=() => {
+        const data = new FormData();
+        data.append("params", "price");
+        data.append("kind", "descending");
+        callAPI({ method: 'POST', url: `hostUrl/sortNFT`, data: data }).then(response => {
+            setSortnftprice(response.payload);
+        });
+    };
+    const FindNFT=()=>{
+        if (min < max)
+        {const data = new FormData();
+            data.append("hotest", "true");
+            data.append("favorites", "true");
+            data.append("latest", "true");
+            callAPI({ method: 'POST', url: `hostUrl/`, data: data }).then(response => {
+                setFilterresualt(response.payload);
+            });}
+    }
 
     const handleChange = (event, newValue) => {
         setTab(newValue);
     }
-
     return (
+        <div>
+            <aside className='browse-sidebar'>
+                <h5 className='filterbox_title'>فیلتر ها</h5>
+                <hr></hr>
+                <div className='filter_gheymat'>
+                    <h5 className='gheymat_title'>قیمت :</h5>
+                    <div>
+                    <label className='min-max-parent'>
+                            <div>
+                              
+                                <TextField fullWidth type="number" label="حداقل" value={min} onChange={e=>setMin(e.target.value)} InputLabelProps={{ shrink:true }}
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">ETH</InputAdornment>,
+                                  }}
+                                />
+                                
+                            </div>
+                    </label>
+                    </div>
+                    <div>
+                    <label className='min-max-parent'>
+                            <div>
+                                
+                                <TextField fullWidth type="number" label="حداکثر" value={max} onChange={e=>setMax(e.target.value)} InputLabelProps={{ shrink:true }} 
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">ETH</InputAdornment>,
+                                  }}
+                                />
+                               
+                            </div>
+                    </label>
+                    </div>
+                    <div>
+                    <Button className="findbtn" onClick={FindNFT}>
+                            پیدا کن
+                        </Button>
+                    </div>
+                </div>
+            </aside>
+        <main className='mainbody'>
         <Container maxWidth="xl">
             <div className="explore-container">
                 <h3 className="explore-title">مجموعه ات را پیدا کن</h3>
@@ -98,9 +171,9 @@ const Explore = () => {
                             })}
                         </Grid>
                     )}
-                    {Resault.data && tab === 'favorites' && (
+                    {Sortcollection.data && tab === 'favorites' && (
                         <Grid container spacing={2}>
-                            {Resault.data.favorites[0].map((item, index) => {
+                            {Sortcollection.data.favorites[0].map((item, index) => {
                                 return (
                                     <Grid item md={2.4} xs={4}>
                                         <Link to={`/collection/${item.id}`} className="nft-collection-parent">
@@ -137,7 +210,10 @@ const Explore = () => {
                 </div>
             </div>
         </Container>
+        </main>
+        </div>
     );
 }
+
 
 export default Explore;
