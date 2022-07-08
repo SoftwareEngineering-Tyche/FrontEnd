ee1a7d9917352aed9eb0bfe1399d30a5cee87653
Ali Fata <a.fata@pinket.com>
2022-07-09 01:09:16 +0430

    add top collections to home page

--- tyche/src/components/top-categories.jsx
+++ tyche/src/components/top-categories.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "../assets/styles/top-categories.scss";
 import Button from '@mui/material/Button';
 import Flickity from 'react-flickity-component';
@@ -8,9 +8,22 @@ import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRound
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, Link } from '@mui/material';
+import { callAPI } from "./api-call";
+import { hostUrl } from "../host-url";
 
-function TopCategories () {
+function TopCategories() {
+    const [categories, setcategories] = useState();
+    useEffect(async () => {
+        const data = new FormData();
+        data.append("hotest", "true");
+        data.append("favorites", "true");
+        data.append("latest", "true");
+        callAPI({ method: 'POST', url: `https://api.ludushub.io/explore`, data: data }).then(response => {
+            setcategories(response.payload.data.latest[0]);
+            console.log("categories", response.payload.data.hotest[0]);
+        });
+    }, []);
     const flickityRef = useRef();
     function handleGotoNextSlide() {
         flickityRef.current.next();
@@ -18,16 +31,22 @@ function TopCategories () {
     function handleGotoPrevSlide() {
         flickityRef.current.previous();
     }
-    function getCards() {
+    function getCard(item, mode) {
         return (
-            <Card sx={{ height:220, borderRadius:'16px', overflow:'unset', margin:'3px', display:'flex', justifyContent:'center', alignItems:'center' }}>
-                <CardActionArea sx={{width:'200px'}}>
-                    <CardMedia sx={{display:'flex', justifyContent:'center', padding:'0px', margin:'0px'}}><img src={imageSample} height={150} width={150}/></CardMedia>
-                    <CardContent sx={{padding:'6px 16px'}}>
-                        <div style={{color:'#2F3A8F'}}>عنوان دسته‌بندی</div>
-                        <div style={{color:'#CDBDFF', fontSize:'small'}}>توضیحات مربوط به دسته بندی</div>
-                    </CardContent>
-                </CardActionArea>
+            <Card sx={{ borderRadius: '16px', overflow: 'unset', margin: '4px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Link underline="none" href={mode === 'product' ? `/product/${item.id}` : `/collection/${item.id}`}>
+                    <CardActionArea sx={{ width: '220px', margin: '8px' }}>
+                        <div style={{ height: '70%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <CardMedia sx={{ display: 'flex', justifyContent: 'center', padding: '4px', margin: '0px' }}>
+                                {mode === 'product' ? <img src={hostUrl + item.image} width={150} height={150} /> : <img src={hostUrl + item.logoimage} width={150} height={150} style={{ borderRadius: '50%' }} />}
+                            </CardMedia>
+                        </div>
+                        <CardContent sx={{ padding: '8px 16px', height: '30%' }}>
+                            <div style={{ color: '#2F3A8F', display: 'flex', justifyContent: 'center' }}>{item.Name}</div>
+                            <div style={{ color: '#CDBDFF', display: 'flex', justifyContent: 'center', fontSize: 'small', textAlign: 'center' }}>{item.Description}</div>
+                        </CardContent>
+                    </CardActionArea>
+                </Link>
             </Card>
         );
     }
@@ -36,17 +55,15 @@ function TopCategories () {
             <span className="title">دسته بندی‌های برتر</span>
             <div className="categories-container overflow-hidden">
                 <Flickity options={flickityOptions} flickityRef={ref => flickityRef.current = ref}>
-                    {getCards()}
-                    {getCards()}
-                    {getCards()}
-                    {getCards()}
-                    {getCards()}
-                    {getCards()}
-                    {getCards()}
+                    {categories && categories.length > 0 &&
+                        categories.map((collection, index) => {
+                            return (getCard(collection, 'collection'));
+                        })
+                    }
                 </Flickity>
                 <div className="buttons">
-                    <Button classes={{root:'btn'}} onClick={handleGotoPrevSlide}><ArrowForwardIosRoundedIcon/></Button>
-                    <Button classes={{root:'btn'}} onClick={handleGotoNextSlide}><ArrowBackIosRoundedIcon/></Button>
+                    <Button classes={{ root: 'btn' }} onClick={handleGotoPrevSlide}><ArrowForwardIosRoundedIcon /></Button>
+                    <Button classes={{ root: 'btn' }} onClick={handleGotoNextSlide}><ArrowBackIosRoundedIcon /></Button>
                 </div>
             </div>
         </div>
@@ -58,7 +75,7 @@ const flickityOptions = {
     draggable: true,
     prevNextButtons: false,
     contain: true,
-    groupCells: window.innerWidth < 768 ? 2 : 5,
+    groupCells: window.innerWidth < 768 ? 1 : 4,
     freeScroll: false,
     cellAlign: 'center',
     wrapAround: true
