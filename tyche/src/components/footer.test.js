22b46444fc2049b6418e25f990ed99f4e2fcedc5
sabaroohandeh <sabarohandeh@yahoo.com>
2022-07-11 16:20:22 +0430

    test

--- tyche/src/components/footer.test.js
+++ tyche/src/components/footer.test.js
@@ -4,8 +4,6 @@ import footer from "./footer";
 test('renders the landing page', () => {
   render(<footer />);
 
-  //   expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
-  //   expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
-  //   expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
-  //   expect(screen.getByRole("img")).toBeInTheDocument();
+  const { asFragment } = render(<footer text='Hello!' />);
+  expect(asFragment()).toMatchSnapshot();
 });
